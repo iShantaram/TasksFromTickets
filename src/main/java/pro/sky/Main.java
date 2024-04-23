87c595f1886b9e295e36dd11ff8ff44eb02bebbd
iShantaram <1agapovav987@gmail.com>
2024-04-23 18:17:13 +0300

    Initial commit

---
+++ src/main/java/pro/sky/Main.java
@@ -0,0 +1,9 @@
+package pro.sky;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //System.out.println(Task2.task());
+        //System.out.println(Greed.greedy(new int[]{1,5,5,5,1}));
+    }
+}
